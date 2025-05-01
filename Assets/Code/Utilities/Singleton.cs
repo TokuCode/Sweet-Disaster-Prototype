44033dfbe4abb44e4bca8d8819f1fe17557e36a9
Adrian Blume <adrianblume1005@gmail.com>
2025-05-01 18:17:58 -0500

    network innit

---
+++ Assets/Code/Utilities/Singleton.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+/// <summary>
+/// A static instance that overrides the current instance.
+/// </summary>
+public abstract class StaticInstance<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+    protected virtual void Awake() => Instance = this as T;
+    protected virtual void OnApplicationQuit()
+    {
+        Instance = null;
+        Destroy(gameObject);
+    }
+}
+
+/// <summary>
+/// Standard singleton, only one instance of this class, doesn't persist.
+/// </summary>
+public abstract class Singleton<T> : StaticInstance<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        base.Awake();
+    }
+}
+
+/// <summary>
+/// Persistent singleton, for audio managers for example.
+/// </summary>
+public abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+}
